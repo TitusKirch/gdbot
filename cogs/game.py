8ecdec07693eaa5ceb4df3def6480f022db50712
Titus Kirch <requests@tkirch.dev>
2020-09-24 21:57:16 +0200

    feat: add gameinfo command

--- cogs/game.py
+++ cogs/game.py
@@ -50,8 +50,52 @@ class Game(commands.Cog, name='Game'):
                                     await member.add_roles(game_role)
 
                                     if guild_history_channel:
-                                        await guild_history_channel.send('{0.mention} ich habe erkannt, dass du "{1.name}" spielst und dir direkt die passende Gruppe zugewiesen.'.format(member, activity))
+                                        await guild_history_channel.send('{0.mention} ich habe erkannt, dass du "**{1.name}**" spielst und dir direkt die passende Gruppe zugewiesen.'.format(member, activity))
                             continue
+
+    @commands.command(hidden=True)
+    @commands.has_permissions(administrator=True)
+    async def gameinfo(self, ctx, member: discord.Member):
+        foundGame = False
+        for activity in member.activities:
+            if activity.type == discord.ActivityType.playing:
+
+                # set found game true
+                foundGame = True
+
+                # create output embed
+                embed = discord.Embed(
+                    colour=discord.Colour.blue(),
+                    title="Spielinformationen",
+                )
+
+                embed.add_field(
+                    name='Name',
+                    value=f'{activity.name}',
+                    inline=False
+                )
+
+                embed.add_field(
+                    name='ApplicationID',
+                    value=f'{activity.application_id}',
+                    inline=False
+                )
+
+                # send message
+                await ctx.send(ctx.author.mention, embed=embed)
+
+        if not foundGame:
+
+            # create output embed
+            embed = discord.Embed(
+                colour=discord.Colour.red(),
+                title=f'Kein Spiel gefunden'
+            )
+            embed.add_field(
+                name="Fehler", value=f'Es konnte kein Spiel bei {member.mention} entdeckt werden.', inline=False)
+
+            # send embed
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
