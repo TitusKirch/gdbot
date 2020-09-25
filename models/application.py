3a6035b9653da2f08924b69a69792bd9a3d932d9
Titus Kirch <requests@tkirch.dev>
2020-09-26 01:38:34 +0200

    feat: create application table

---
+++ models/application.py
@@ -0,0 +1,15 @@
+import json
+import os
+
+from typing import List
+
+from sqlalchemy import Column, String, Boolean, Text, Integer
+
+from db import db_session
+from models.base import Base
+
+
+class Application(Base):
+    __tablename__ = 'applications'
+
+    applicationID = Column(Integer, autoincrement=True, primary_key=True)
